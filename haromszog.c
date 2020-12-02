e0ed78a9cb82e2b2d38ddaaca40ce4b09496a0a9
panky988 <panky988@gmail.com>
2020-12-02 22:48:06 +0100

    Create haromszog.c

---
+++ haromszog.c
@@ -0,0 +1,29 @@
+//Készíts programot, ami egy háromszög oldalainak hosszát bekérve kiszámítja, hogy szerkeszthető-e a háromszög!
+
+#include <stdio.h>
+
+int main ()
+{
+    int i;
+    float oldalak[3];
+    printf("\n  Beadandó feladatok / Szelekció / 4.feladat\n");
+    printf("  -------------------------------------------\n");
+    for (i = 0; i < 3; i++)
+    {
+        printf("  Kérem a(z) %d. oldal hosszát (cm): ", i+1);
+        scanf ("%f", &oldalak[i]);
+    }
+    printf ("    A: %.2fcm\n", oldalak[0]);
+    printf ("    B: %.2fcm\n", oldalak[1]);
+    printf ("    C: %.2fcm\n", oldalak[2]);
+      
+    if(oldalak[0]+oldalak[1]>oldalak[2] && oldalak[0]+oldalak[2]>oldalak[1] && oldalak[1]+oldalak[2]>oldalak[0])
+    {
+        printf("  A háromszög szerkeszthető!\n\n");
+    }
+    else
+    {
+        printf("  A háromszög nem szerkeszthető\n\n");
+    }
+    return 0;
+}
