9d15637bbeb5d65d5c9ded5a34f517cbb1397fe7
panky988 <panky988@gmail.com>
2020-12-02 22:35:56 +0100

    Create halmazallapot.c

---
+++ halmazallapot.c
@@ -0,0 +1,25 @@
+//Kérd be a víz hőfokát, állapítsd meg belőle a halmazállapotát!
+
+#include <stdio.h>
+
+int main()
+{
+    printf("\n  Beadandó feladatok / Szelekció / 1.feladat\n");
+    printf("  -------------------------------------------\n");
+    int hofok;
+    printf("  Kérem a víz hőfokát: ");
+    scanf("%d", &hofok);
+    if(hofok<=0)
+    {
+        printf("  Jég\n\n");
+    }
+    else if(hofok>=100)
+    {
+        printf("  Gőz\n\n");
+    }
+    else
+    {
+        printf("  Víz\n\n");
+    }
+    return 0;
+}
